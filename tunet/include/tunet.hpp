ad95df7bf5a6f846227cf82c75b2a8f0a4989a24
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-10-18 15:01:02 +0800

    Added logout method and whoami for fetching currently connected account

--- tunet/include/tunet.hpp
+++ tunet/include/tunet.hpp
@@ -8,13 +8,21 @@
 #include <boost/asio/io_context.hpp>
 #include <boost/utility/string_view.hpp>
 #include <boost/optional.hpp>
-#include "payloads/status_response.hpp"
-#include "payloads/login_response.hpp"
+#include <payloads/status_response.hpp>
+#include <payloads/login_response.hpp>
+#include <payloads/logout_response.hpp>
+#include <error_code.hpp>
 
 namespace tunet {
     boost::optional<tunet::payloads::login_response>
-    login(boost::asio::io_context &ioc, std::vector<std::pair<boost::string_view, boost::string_view>> credentials,
+    login(boost::asio::io_context &ioc, boost::string_view username, boost::string_view password,
           boost::system::error_code &ec) noexcept;
+
+    boost::optional<tunet::payloads::logout_response>
+    logout(boost::asio::io_context &ioc, boost::string_view username, boost::system::error_code &ec) noexcept;
+
+    boost::optional<std::string>
+    whoami(boost::asio::io_context &ioc, boost::system::error_code &ec) noexcept;
 
     boost::optional<tunet::payloads::status_response>
     status(boost::asio::io_context &ioc, boost::system::error_code &ec) noexcept;
