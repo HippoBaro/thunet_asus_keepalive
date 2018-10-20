98e69658d851b02fb4e8c17cd47a151fbdcb0a6f
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-10-20 18:58:52 +0800

    Dynamic scheduling for periodic tasks

--- src/keepalive_task.cpp
+++ src/keepalive_task.cpp
@@ -39,57 +39,73 @@ keepalive_task::login_account(boost::asio::io_context &ioc, boost::string_view u
     return true;
 }
 
-int keepalive_task::task(boost::asio::io_context &ioc) {
-    boost::system::error_code ec;
-
+std::pair<keepalive_task::keepalive_sleep, bool>
+keepalive_task::account_roundrobin(boost::asio::io_context &ioc, boost::system::error_code &ec) {
     log("Finding currently logged in user", log_level::debug);
     auto user = tunet::whoami(ioc, ec);
     if (ec) {
         log("Fetching currently logged in user failed: " + ec.message(), log_level::error);
-        return 120;
+        return std::make_pair(keepalive_sleep::long_sleep, true);
     }
 
     if (user) {
         log("Fetching connection status for account " + *user, log_level::debug);
         auto connection_status = tunet::status(ioc, ec);
         if (ec) {
-            log("Fetching connection status failed: " + ec.message(), log_level::error);
+            log("Fetching connection status failed: " + ec.message() + ". Assuming no internet access",
+                log_level::error);
+            ec.clear();
         }
 
         if (connection_status) {
-            log("Currently online with internet access as " + connection_status->username);
-            return 120;
+            log("Currently online with internet access as " + connection_status->username, log_level::debug);
+            return std::make_pair(keepalive_sleep::long_sleep, true);
         }
         else {
             log("Not online. Logging out " + *user, log_level::debug);
             tunet::logout(ioc, *user, ec);
             if (ec) {
                 log("Logout failed: " + ec.message(), log_level::error);
-                return 120;
+                return std::make_pair(keepalive_sleep::long_sleep, true);
             }
-            return 20;
+            return std::make_pair(keepalive_sleep::short_sleep, false);
         }
     }
     else {
-        log("Not logged in.", log_level::debug);
-
         if (current_account >= std::end(settings.accounts)) {
             current_account = std::begin(settings.accounts);
         }
 
+        log("No account currently logged in, trying " + current_account->first.to_string(), log_level::debug);
+
         if (login_account(ioc, current_account->first, current_account->second, ec)) {
             log("Successfully logged in as " + current_account->first.to_string(), log_level::info);
-            return 120;
+            return std::make_pair(keepalive_sleep::long_sleep, true);
         }
         log("Account " + current_account->first.to_string() +
             " does not have internet access. Retrying with next account...", log_level::info);
         ++current_account;
-        return 20;
+        return std::make_pair(keepalive_sleep::short_sleep, false);
+    }
+}
+
+
+int keepalive_task::task(boost::asio::io_context &ioc) {
+    boost::system::error_code ec;
+    auto res = account_roundrobin(ioc, ec);
+    if (!res.second) {
+        previous_sleep = 0;
+        return static_cast<int>(res.first);
+    } else if (previous_sleep >= 5 * static_cast<int>(res.first)) {
+        return previous_sleep;
+    } else {
+        previous_sleep += 2 * static_cast<int>(res.first);
+        return previous_sleep;
     }
 }
 
 boost::string_view keepalive_task::name() {
-    return "TUNet-Keepalive";
+    return "TUNetKeepalive";
 }
 
 keepalive_task::~keepalive_task() {
--- src/keepalive_task.hpp
+++ src/keepalive_task.hpp
@@ -21,10 +21,20 @@ struct keepalive_task : public periodic_task {
     boost::string_view name() override;
 
 private:
+
+    enum class keepalive_sleep : int {
+        long_sleep = 5,
+        short_sleep = 12
+    };
+
     bool login_account(boost::asio::io_context &ioc, boost::string_view username, boost::string_view password,
                        boost::system::error_code &ec);
 
+    std::pair<keepalive_sleep, bool> account_roundrobin(boost::asio::io_context &ctx, boost::system::error_code &ec);
+
     user_settings::account_container::const_iterator current_account;
+
+    int previous_sleep = 0;
 };
 
 
