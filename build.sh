af6b213481d8c60cc8282cdd21f295ccbd7ba4f1
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-10-24 02:08:42 +0800

    OpenSSL as shared dependency

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker run -v `pwd`:/home --tty --rm hippobaro/asuswrt-musl-crosscompile:latest sh -c "mkdir build ; cd build && cmake -DCMAKE_TOOLCHAIN_FILE=../ASUS-RT-AC51U-toolchain.cmake -DSTRIP=ON -GNinja .. && ninja"
+docker run -v `pwd`:/home --tty --rm hippobaro/asuswrt-musl-crosscompile:latest sh -c "mkdir build ; cd build && cmake -DOPENSSL_ROOT_DIR=/usr/local/ssl/ -DCMAKE_TOOLCHAIN_FILE=../ASUS-RT-AC51U-toolchain.cmake -DSTRIP=ON -GNinja .. && ninja"
